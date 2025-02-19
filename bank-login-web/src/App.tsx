5f3708afa06abc0123d20b4a69744a6d2a1b5ea9
CARLOS MANOEL WENDORFF <carlos.wendorff@catolicasc.edu.br>
2025-02-19 20:42:45 -0300

    added number selection

--- bank-login-web/src/App.tsx
+++ bank-login-web/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState("");
+  
+  const keys = [
+    ["3", "9"], ["0", "5"], ["2", "8"],
+    ["1", "4"], ["6", "7"], ["⌫"]
+  ];
+
+  const handleKeyPress = (value:string) => {
+    if (value === "⌫") {
+      setInputValue(inputValue.slice(0, -1));
+    } else {
+      setInputValue(inputValue + value);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container" >
+      <h2>Teclado Virtual</h2>
+      <input type="password" value={inputValue} readOnly className="input" />
+      <div className="keyboard">
+        {keys.map((pair, index) => (
+          <button key={index} onClick={() => handleKeyPress(pair[0])} className="key">
+            {pair[0]} ou {pair[1]}
+          </button>
+        ))}
+        <button onClick={() => handleKeyPress("⌫")} className="key delete">⌫</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <button className="access">Acessar</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
